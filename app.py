a56e886bb0b1b863169f79f2199c88a921b802c3
Ritu146 <72628067+Ritu146@users.noreply.github.com>
2026-04-21 15:04:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,11 +19,20 @@ st.title("💳 AI Credit Risk App")
 # --------------------------
 st.header("📊 Credit Risk Prediction")
 
-age = st.number_input("Age", key="age_input")
-income = st.number_input("Monthly Income", key="income_input")
+# age = st.number_input("Age", key="age_input")
+# income = st.number_input("Monthly Income", key="income_input")
+age = st.number_input("Age", key="age")
+NumberOfTime30_59DaysPastDueNotWorse = st.number_input("30-59 Days Past Due", key="dpd_30")
+DebtRatio = st.number_input("Debt Ratio", key="debt_ratio")
+income = st.number_input("Monthly Income", key="income")
+NumberOfOpenCreditLinesAndLoans = st.number_input("Open Credit Lines", key="open_credit")
+NumberOfTimes90DaysLate = st.number_input("90 Days Late", key="late_90")
+NumberRealEstateLoansOrLines = st.number_input("Real Estate Loans", key="real_estate")
+NumberOfTime60_89DaysPastDueNotWorse = st.number_input("60-89 Days Past Due", key="dpd_60")
+NumberOfDependents = st.number_input("Dependents", key="dependents")
 
 if st.button("Predict", key="predict_btn"):
-    data = [0, 0, age, 0, 0, income, 0, 0, 0, 0, 0]
+    data = [0, 0, age, NumberOfTime30_59DaysPastDueNotWorse, DebtRatio, income, NumberOfOpenCreditLinesAndLoans, NumberOfTimes90DaysLate, NumberRealEstateLoansOrLines, NumberOfTime60_89DaysPastDueNotWorse, NumberOfDependents]
     result = predict(data)
     st.success(f"Default Probability: {result}")
 
